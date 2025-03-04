df0208de725106f8e2a265ce2bf49f4d0ddd6db6
Md Woliul Hasan <hmwoliul@gmail.com>
2025-03-05 00:47:27 +0600

    Move to Exercise Folder, Separate day wise, Change Age to Current Age in day2.py

--- Exercise/Day2/day2.py
+++ Exercise/Day2/day2.py
@@ -1,20 +1,20 @@
 #Print name,age and favourite color
-"""
+
 name="Pijush"
 age="30"
 favourite_color="black"
 print(name,age,favourite_color)
-"""
+
 #Taking a number and double it
-"""
+
 f_number=int(input("Enter your favourite number: "))
 
 doubel=f_number*2
 print(doubel)
-"""
+
 #birth year calculation
 birth_year=int(input("Enter your birth year: "))
 current_year=int(input("Enter current year: "))
 
-age=current_year-birth_year
-print(age)
+current_age=current_year-birth_year #Change Age to Current Age
+print(current_age)

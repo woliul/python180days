9435de6685ab23f875c452a198eab0cfd5252e34
Md Woliul Hasan <hmwoliul@gmail.com>
2025-03-05 00:58:53 +0600

    Update day3exercise4.py

--- Exercise/Day3/day3 execrice4.py
+++ Exercise/Day3/day3 execrice4.py
@@ -1,12 +1,7 @@
 #Execrise 4
 #Store Personal Information
-from tkinter.font import names
 
 information=(input("Name:"),int(input("Date of Birth:")),str(input("Favourite Color:")))
-
-
-
-
 
 print(information[0])
 print(information[1])

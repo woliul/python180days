8c8ff815163ed80618afe93137210ad10cd55bfa
Pijush Mazumder <107364525+jushi2@users.noreply.github.com>
2025-03-08 00:19:17 +0600

    Add files via upload

--- Exercise/Day4/day4 exerise2.py
+++ Exercise/Day4/day4 exerise2.py
@@ -1,1 +1,11 @@
+#Modify the dictionary by adding a new key
+student_info={
+    "Name":str(input("Enter your Name:")),
+    "Age":int(input("Enter your Age:")),
+    "Grade":float(input("Enter your Grade:")),
+    "Subject":str(input("Enter your Subject:")),
+}
 
+student_info["hobby"]=str(input("Enter your hobby:"))
+
+print(student_info)
